56a3ae7ff8f3bf7e11c065b6e1d71d1125c5e33c
kaye-s <kayes@uwm.edu>
2024-11-23 19:33:07 -0600

    A few login tests, updated urls to actually point to stuff for testing and added a basic login view for testing

--- TAScheduler/urls.py
+++ TAScheduler/urls.py
@@ -1,1 +1,12 @@
+
+from django.contrib import admin
 from django.urls import path
+from TAScheduler.views import AccountManagementView
+from TAScheduler.views import Login
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #Temporarily routing home page as Account Management so we can see what it looks like
+    path('',Login.as_view()),
+    path('AccountManagement/',AccountManagementView.as_view()),
+]
