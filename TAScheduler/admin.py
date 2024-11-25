2e81cb711d91a23fe6f00d7be1b9a222165b3ae8
Israel <adigun.jolly@gmail.com>
2024-11-24 23:39:50 -0600

    updated commit

--- TAScheduler/admin.py
+++ TAScheduler/admin.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
-from .models import User, Supervisor  # Uncomment Instructor and TA when ready
+from .models import *
 from .forms import SupervisorAdminForm, UserForm
 
 # Base User Admin
@@ -42,17 +42,73 @@ class SupervisorAdmin(admin.ModelAdmin):
         return obj.user.lname
     get_lname.short_description = 'Last Name'
 
+# Instructor Admin
+@admin.register(Instructor)
+class InstructorAdmin(admin.ModelAdmin):
+    list_display = ('get_email', 'get_fname', 'get_lname', 'instructor_dept')
+    search_fields = ('user__email', 'user__fname', 'user__lname', 'instructor_dept')
 
-# Uncomment these when ready
-# # Instructor Admin
-# @admin.register(Instructor)
-# class InstructorAdmin(admin.ModelAdmin):
-#     list_display = ('get_email', 'get_fname', 'get_lname', 'instructor_dept')
-#     search_fields = ('user__email', 'user__fname', 'user__lname', 'instructor_dept')
+    def get_email(self, obj):
+        return obj.user.email
+    get_email.short_description = 'Email'
 
-# # TA Admin
-# @admin.register(TA)
-# class TAAdmin(admin.ModelAdmin):
-#     list_display = ('get_email', 'get_fname', 'get_lname', 'ta_dept')
-#     search_fields = ('user__email', 'user__fname', 'user__lname', 'ta_dept')
+    def get_fname(self, obj):
+        return obj.user.fname
+    get_fname.short_description = 'First Name'
+
+    def get_lname(self, obj):
+        return obj.user.lname
+    get_lname.short_description = 'Last Name'
+
+# TA Admin
+@admin.register(TA)
+class TAAdmin(admin.ModelAdmin):
+    list_display = ('get_email', 'get_fname', 'get_lname', 'ta_dept')
+    search_fields = ('user__email', 'user__fname', 'user__lname', 'ta_dept')
+
+    def get_email(self, obj):
+        return obj.user.email
+    get_email.short_description = 'Email'
+
+    def get_fname(self, obj):
+        return obj.user.fname
+    get_fname.short_description = 'First Name'
+
+    def get_lname(self, obj):
+        return obj.user.lname
+    get_lname.short_description = 'Last Name'
+
+from django.contrib import admin
+from .models import Lab, Section, Lecture, Course
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('course_name', 'course_identifier', 'course_dept', 'course_credits', 'super_id')
+    search_fields = ('course_name', 'course_identifier', 'course_dept')
+    list_filter = ('course_dept',)
+    ordering = ('course_name',)
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ('section_num', 'section_course')
+    search_fields = ('section_num', 'section_course__course_name')
+    list_filter = ('section_course__course_dept',)
+    ordering = ('section_num',)
+
+
+@admin.register(Lab)
+class LabAdmin(admin.ModelAdmin):
+    list_display = ('lab_section', 'lab_ta', 'days_of_week', 'lab_startTime', 'lab_endTime')
+    search_fields = ('lab_section__section_course__course_name', 'lab_ta__user__email')
+    list_filter = ('days_of_week',)
+    ordering = ('lab_startTime',)
+
+
+@admin.register(Lecture)
+class LectureAdmin(admin.ModelAdmin):
+    list_display = ('lecture_section', 'lecture_instructor', 'days_of_week', 'lecture_startTime', 'lecture_endTime')
+    search_fields = ('lecture_section__section_course__course_name', 'lecture_instructor__user__email')
+    list_filter = ('days_of_week',)
+    ordering = ('lecture_startTime',)
 
--- TAScheduler/views.py
+++ TAScheduler/views.py
@@ -1,33 +1,41 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, logout
+from django.contrib.auth.views import LoginView
 from django.utils.decorators import method_decorator
+from django.urls import reverse_lazy
 from django.views import View
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from TAScheduler.utils.auth import group_required  # Import the group_required decorator
 from TAScheduler.utils.account_management import create_user_account, edit_user_account, delete_user_account  # Utility functions
+from TAScheduler.models import Supervisor, TA, Instructor
 
 
 User = get_user_model()
 
-@method_decorator([group_required('Supervisor'),  login_required], name='dispatch')
+class CustomLoginView(LoginView):
+    template_name = 'login.html'  # Login page template
+    redirect_authenticated_user = True  # Redirect if already logged in
+
+    # Redirect all users to a single page after login
+    def get_success_url(self):
+        return reverse_lazy('account-management') 
+
+
+
+#Handles account management tasks accessible only to Supervisors.
+@method_decorator([login_required(login_url="/"), group_required('Supervisor')], name='dispatch')
 class AccountManagementView(View):
-    """
-    Handles account management tasks accessible only to Supervisors.
-    """
-
+   
+    #Renders the account management page with users who are not superusers.
     def get(self, request):
-        """
-        Renders the account management page with users who are not superusers.
-        """
-        users = User.objects.filter(is_superuser=False)  # Exclude superusers
-        return render(request, 'AccountManagement.html', {'users': users})
-
+        s,i,t = Supervisor.objects.all(), Instructor.objects.all(), TA.objects.all()
+        return render(request, 'AccountManagement.html', {'supervisors': s, "tas":t, "instructors":i, "role":"Supervisor"})
+    
+    #Handles account management actions: create, edit, or delete a user account.
     def post(self, request):
-        """
-        Handles account management actions: create, edit, or delete a user account.
-        """
         action = request.POST.get('action')
+       
 
         if action == 'create':
             return create_user_account(request)
