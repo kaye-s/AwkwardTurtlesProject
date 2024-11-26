c98c34d3b2c76cee7f7d68a6bf53dbb8fea4daa3
Israel <adigun.jolly@gmail.com>
2024-11-26 01:07:43 -0600

    updated commit

--- TAScheduler/urls.py
+++ TAScheduler/urls.py
@@ -7,3 +7,4 @@ urlpatterns = [
     path("", CustomLoginView.as_view(), name="login"),
     path("account-management/", AccountManagementView.as_view(), name="account-management"),
     path("logout/",LogoutView.as_view(), name="logout")
+]
