cfe2880cc8545636af966e239d0b6ff0c432b2ec
Israel <adigun.jolly@gmail.com>
2024-11-26 20:38:53 -0600

    added validation for address on edit form

--- TAScheduler/static/js/formValidation.js
+++ TAScheduler/static/js/formValidation.js
@@ -1,10 +1,16 @@
-function initializeAutocomplete() {
+function initializeAutocomplete (){
     const address1Input = document.getElementById("address1");
+    const editAddress1Input = document.getElementById("edit_address1");
     
   
     const autocomplete = new google.maps.places.Autocomplete(address1Input, {
       componentRestrictions: { country: "us" }, 
     });
+
+    const editAutoComplete =  new google.maps.places.Autocomplete(editAddress1Input, {
+      componentRestrictions: { country: "us" }, 
+    });
+
 
     autocomplete.addListener("place_changed", () => {
       const place = autocomplete.getPlace();
@@ -16,21 +22,37 @@ function initializeAutocomplete() {
 
       console.log("Selected place:", place);
     });
+
+    editAutoComplete.addListener("place_changed", () => {
+      const place = autocomplete.getPlace();
+
+      if (!place.geometry) {
+        console.log("No details available for the input: '" + place.name + "'");
+        return;
+      }
+
+      console.log("Selected place:", place);
+    });
+}
+
+const phoneInput1 = document.getElementById('phone_number');
+const phoneInput2 = document.getElementById('edit_phone_number');
+
+phoneInput1.addEventListener('input', validated_phone);
+phoneInput2.addEventListener('input', validated_phone);
+
+
+function validated_phone(e){
+  let input = e.target.value;
+  console.log("phone_input_debug");
+
+  input = input.replace(/\D/g, '');
+
+  if (input.length > 3 && input.length <= 6) {
+      input = `${input.slice(0, 3)}-${input.slice(3)}`;
+  } else if (input.length > 6) {
+      input = `${input.slice(0, 3)}-${input.slice(3, 6)}-${input.slice(6, 10)}`;
   }
-
-const phoneInput = document.getElementById('phone_number');
-
-phoneInput.addEventListener('input', (e) => {
-    let input = e.target.value;
-    console.log("phone_input_debug");
-
-    input = input.replace(/\D/g, '');
-
-    if (input.length > 3 && input.length <= 6) {
-        input = `${input.slice(0, 3)}-${input.slice(3)}`;
-    } else if (input.length > 6) {
-        input = `${input.slice(0, 3)}-${input.slice(3, 6)}-${input.slice(6, 10)}`;
-    }
-    e.target.value = input;
-});
+  e.target.value = input;
+}
 
