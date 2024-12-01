7c4faa12cba173430b9dfbfeeff56c539481055d
kaye-s <kayes@uwm.edu>
2024-11-30 20:43:32 -0600

    Test skeletons

---
+++ TAScheduler/tests/AcceptanceTests/test_create_courses.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+from django.db.utils import IntegrityError
+from django.contrib.auth import get_user_model
+from django.contrib.messages import get_messages
+from django.contrib.auth.models import Group
