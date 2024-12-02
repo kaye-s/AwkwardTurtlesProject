9743c13e058251ea1810043a32c15bcd087e4b7b
kaye-s <kayes@uwm.edu>
2024-12-02 17:19:30 -0600

    simple fix to create - edit and delete accounts acceptance tests written

--- TAScheduler/tests/AcceptanceTests/test_edit_accounts.py
+++ TAScheduler/tests/AcceptanceTests/test_edit_accounts.py
@@ -1,102 +1,149 @@
-# from django.contrib.auth import get_user_model
-# from django.test import TestCase
-# from django.contrib.auth.models import Group
+from django.test import TestCase
+from django.db.utils import IntegrityError
+from django.contrib.auth import get_user_model
+from django.contrib.messages import get_messages
+from django.contrib.auth.models import Group
+from TAScheduler.models import TA, Instructor
 
-# User = get_user_model()
+User = get_user_model()
 
-# #CANNOT RUN THESE TESTS UNTIL I FIGURE OUT WHAT THE PATH IS FOR THE EDIT ACCOUNTS FORM
-# class AccountManagementTests(TestCase):
+class AccountManagementEditTests(TestCase):
 
-#     def setUp(self):
-#         self.supervisor_user = User.objects.create_user(
-#             email='supervisor@example.com',
-#             password='superpassword123',
-#             fname='Supervisor',
-#             lname='User',
-#             address='123 Supervisor Lane',
-#             phone_number='1234567890'
-#         )
+    def setUp(self):
+        self.supervisor_user = User.objects.create_user(
+            email='supervisor@example.com',
+            password='superpassword123',
+            fname='Supervisor',
+            lname='User',
+            address='123 Supervisor Lane',
+            phone_number='1234567890'
+        )
 
-#         supervisor_group, _ = Group.objects.get_or_create(name='Supervisor')
-#         self.supervisor_user.groups.add(supervisor_group)
-#         self.supervisor_user.save()
+        supervisor_group, _ = Group.objects.get_or_create(name='Supervisor')
+        self.supervisor_user.groups.add(supervisor_group)
+        self.supervisor_user.save()
 
-#         self.test_user = User.objects.create_user(
-#             email='user@example.com',
-#             password='userpassword123',
-#             fname='User',
-#             lname='Example',
-#             address='123 Example Lane',
-#             phone_number='9876543210'
-#         )
+        #Ta to test editing
+        self.ta_user = User.objects.create_user(
+            email='ta@example.com',
+            password='tapassword123',
+            fname='TA',
+            lname='User',
+            address='456 TA Road',
+            phone_number='0987654321'
+        )
 
-#     def test_supervisor_edit_user_success(self):
-#         self.client.login(email='supervisor@example.com', password='superpassword123')
+        ta_group, _ = Group.objects.get_or_create(name='TA')
+        self.ta_user.groups.add(ta_group)
+        self.ta_user.save()
 
-#         data = {
-#             'user_id': self.test_user.id,
-#             'fname': 'Updated',
-#             'lname': 'User'
-#         }
+    def test_supervisor_edit_nothing(self):
+        self.client.login(email='supervisor@example.com', password='superpassword123')
 
-#         response = self.client.post("AccountManagement/", data)
+        response = self.client.get("/account-management/")
+        self.assertEqual(response.status_code, 200)
 
-#         updated_user = User.objects.get(id=self.test_user.id)
-#         self.assertEqual(updated_user.fname, 'Updated')
-#         self.assertEqual(updated_user.lname, 'User')
+        data = {
+            'action' : 'edit'
+        }
+        response = self.client.post("/account-management/", data)
+        self.assertEqual(response.status_code, 302) #status is a type 3XX cause our view redirects back to itself
 
-#         self.assertContains(response, "User user@example.com has been updated.")
+    def test_supervisor_edit_user_fname(self):
+        self.client.login(email='supervisor@example.com', password='superpassword123')
 
-#         self.assertRedirects(response, "AccountManagement/")
+        response = self.client.get("/account-management/")
+        self.assertEqual(response.status_code, 200)
+        #taEmail = self.ta_user.email
+        #id = TA.objects.get(email = taEmail)
+        #cur_user = id.user_id
+        data = {
+            'email': 'ta@example.com',
+            'fname': 'John',
+            'action' : 'edit'
+        }
+        response = self.client.post("/account-management/", data)
+        self.assertEqual(response.status_code, 302) #status is a type 3XX cause our view redirects back to itself
 
-#     def test_supervisor_edit_user_incomplete_form(self):
-#         self.client.login(email='supervisor@example.com', password='superpassword123')
+        self.assertEqual('John', self.ta_user.fname)
 
-#         data = {
-#             'user_id': self.test_user.id,
-#             'lname': 'Updated'
-#         }
-#         response = self.client.post("AccountManagement/", data)
+    def test_supervisor_edit_user_role(self):
+        self.client.login(email='supervisor@example.com', password='superpassword123')
 
-#         self.assertFormError(response, 'form', 'fname', 'This field is required.')
+        response = self.client.get("/account-management/")
+        self.assertEqual(response.status_code, 200)
 
-#         unchanged_user = User.objects.get(id=self.test_user.id)
-#         self.assertEqual(unchanged_user.fname, 'User')
+        data = {
+            'email': 'ta@example.com',
+            'role': 'Instructor',
+            'action' : 'edit'
+        }
+        response = self.client.post("/account-management/", data)
+        self.assertEqual(response.status_code, 302) #status is a type 3XX cause our view redirects back to itself
 
-#     def test_supervisor_edit_user_non_unique_email(self):
-#         self.new_user = User.objects.create_user(
-#             email='newuser@example.com',
-#             password='newuserpassword123',
-#             fname='New',
-#             lname='User',
-#             address='789 New Road',
-#             phone_number='1122334455'
-#         )
+        self.assertEqual('Instructor', self.ta_user.role)
 
-#         self.client.login(email='supervisor@example.com', password='superpassword123')
+    def test_supervisor_edit_user_pass(self):
+        self.client.login(email='supervisor@example.com', password='superpassword123')
 
-#         data = {
-#             'user_id': self.test_user.id,
-#             'email': 'newuser@example.com',
-#             'fname': 'Duplicate',
-#             'lname': 'User'
-#         }
-#         response = self.client.post("AccountManagement/", data)
+        response = self.client.get("/account-management/")
+        self.assertEqual(response.status_code, 200)
 
-#         self.assertContains(response, 'A user with that email address already exists.')
+        data = {
+            'email': 'ta@example.com',
+            'password': 'tapassword456',
+            'action' : 'edit'
+        }
+        response = self.client.post("/account-management/", data)
+        self.assertEqual(response.status_code, 302) #status is a type 3XX cause our view redirects back to itself
 
-#         unchanged_user = User.objects.get(id=self.test_user.id)
-#         self.assertEqual(unchanged_user.email, 'user@example.com')
+        self.assertEqual('tapassword456', self.ta_user.password)
 
-#     def test_ta_edit_user_access_denied(self):
-#         self.client.login(email='ta@example.com', password='tapassword123')
+    def test_supervisor_edit_user_email(self):
+        self.client.login(email='supervisor@example.com', password='superpassword123')
 
-#         data = {
-#             'user_id': self.test_user.id,
-#             'fname': 'Unauthorized',
-#             'lname': 'Edit'
-#         }
+        response = self.client.get("/account-management/")
+        self.assertEqual(response.status_code, 200)
 
-#         response = self.client.post("AccountManagement/", data)
+        data = {
+            'email': 'email@example.com',
+            'action' : 'edit'
+        }
+        response = self.client.post("/account-management/", data)
+        self.assertEqual(response.status_code, 302) #status is a type 3XX cause our view redirects back to itself
 
-#         self.assertRedirects(response, "/")
+        messages = list(get_messages(response.wsgi_request))
+
+        self.assertTrue(
+            any("Cannot edit email, please instead create a new account for the user" in str(message) for message in messages),
+            "Expected a message - inability to edit email")
+
+    def test_ta_access_account_management(self):
+        self.client.login(email='ta@example.com', password='tapassword123')
+
+        response = self.client.get("/account-management/")
+
+        self.assertEqual(response.status_code, 403)
+        self.assertTemplateUsed(response,
+                                '403.html')
+
+
+    def test_instructor_access_account_management(self):
+        User.objects.create_user(
+            email='instructor@example.com',
+            password='instructorpassword123',
+            fname='Instructor',
+            lname='User',
+            address='789 Instructor Lane',
+            phone_number='1239874560'
+        )
+
+        self.client.login(email='instructor@example.com', password='instructorpassword123')
+
+        response = self.client.get("/account-management/")
+
+        self.assertEqual(response.status_code, 403)
+        self.assertTemplateUsed(response,
+                                '403.html')
+
+
