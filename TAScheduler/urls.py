e0a00ed92b5942459b7cd5fec600656ed39d62eb
manvirk0215 <manvir@uwm.edu>
2024-12-02 22:46:16 -0600

    Add course management template for supervisors
     a new HTML template, courses_supervisor.html, for managing courses available to supervisors.  includes a table to display existing courses and provides options to edit or delete each course . a button for creating new courses

--- TAScheduler/urls.py
+++ TAScheduler/urls.py
@@ -6,5 +6,6 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path("", CustomLoginView.as_view(), name="login"),
     path("account-management/", AccountManagementView.as_view(), name="account-management"),
-    path("logout/",LogoutView.as_view(), name="logout")
+    path("logout/",LogoutView.as_view(), name="logout"),
+
 ]
