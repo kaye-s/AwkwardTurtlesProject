1df8e6556cd39199de564482724340603b573ae3
manvirk0215 <manvir@uwm.edu>
2024-12-04 21:56:14 -0600

    Updated course creation and deletion tests to include the correct URL routing and redirect changed assertions to ensure correct  paths that were added. also imported course model

--- TAScheduler/views.py
+++ TAScheduler/views.py
@@ -95,6 +95,7 @@ def create_course(request):
         course_dept = request.POST.get('course_dept')
         course_credits = request.POST.get('course_credits')
 
+
         Course.objects.create(
             course_name=course_name,
             course_identifier=course_identifier,
@@ -102,6 +103,7 @@ def create_course(request):
             course_credits=course_credits,
             super_id=request.user.supervisor
         )
+
         return redirect('courses-supervisor')
 
 # Edit an existing course
