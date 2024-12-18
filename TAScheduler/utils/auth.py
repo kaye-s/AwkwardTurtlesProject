a30f952ce2c754dca8b7579bce6ee03eeb97495a
Israel <adigun.jolly@gmail.com>
2024-12-17 18:58:24 -0600

    method decorator implementation added

--- TAScheduler/utils/auth.py
+++ TAScheduler/utils/auth.py
@@ -11,3 +11,14 @@ def group_required(group_name):
         raise PermissionDenied  # Raise 403 error for unauthorized users
 
     return user_passes_test(in_group)
+
+def method_group_required(group_name):
+    def wrap(func):
+        def wrapper(self, request, *args, **kwargs):
+            if request.user.groups.filter(name=group_name).exists():
+                return func(self, request, *args, **kwargs)
+            else:
+                raise PermissionDenied
+            
+        return wrapper
+    return wrap
