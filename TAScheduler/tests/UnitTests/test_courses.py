37107f1f6008bfcb98eca9d2154b8431e2f38cb4
kaye-s <kayes@uwm.edu>
2024-12-18 21:46:33 -0600

    edit course first test

--- TAScheduler/tests/UnitTests/test_courses.py
+++ TAScheduler/tests/UnitTests/test_courses.py
@@ -1,3 +1,5 @@
+from cgitb import reset
+
 from django.test import TestCase
 from unittest.mock import MagicMock, patch
 from django.contrib.auth.models import User
@@ -30,6 +32,28 @@ class AccountManagementCreateTests(TestCase):
         self.supervisor_user = Supervisor(user=self.supervisor_user, admin_dept="dept")
         self.supervisor_user.save()
 
+        self.instructor_user = User.objects.create_user(
+            email='instructor@example.com',
+            password='instructorpassword123',
+            fname='Instructor',
+            lname='User',
+            address='456 TA Road',
+            phone_number='0987654321'
+        )
+
+        self.instructor_user.save()
+        self.instructor_user = Instructor(user=self.instructor_user, instructor_dept="dept")
+        self.instructor_user.save()
+
+        self.course_test = Course.objects.create(
+            super_id=self.supervisor_user,
+            course_name='CS600',
+            course_identifier='600',
+            course_dept='Computer Science',
+            course_credits=3,
+        )
+        self.course_test.save()
+
     @patch('django.contrib.messages.error')
     def test_create_course_success(self, mock_message):
         post_data = {
@@ -55,266 +79,67 @@ class AccountManagementCreateTests(TestCase):
         mock_message.assert_not_called()
 
     @patch('django.contrib.messages.error')
-    def test_create_user_invalid_email_empty(self, mock_message):
+    def test_create_course_success_instructor(self, mock_message):
         post_data = {
-            'email': 'None',  # Invalid email
-            'fname': 'John',
-            'lname': 'Doe',
-            'role': 'TA',
-            'dept': 'Computer Science',
-            'phone_number': '1234567890',
-            'address1': '123 Main St',
-            'address2': 'Some City',
-            'password': 'password123',
+            'course_name': 'Test Course',
+            'course_identifier': '150',
+            'course_dept': 'Computer Science',
+            'course_credits': 3,
+            'instructor': self.instructor_user.user
         }
 
         request = MagicMock()
         request.POST = post_data
+        request.user = self.supervisor_user.user
 
-        create_user_account(request)
+        create_course(request)
 
-        mock_message.assert_called_with(request, "Email cannot be empty")
+        course = Course.objects.get(course_name='Test Course')
 
-    @patch('django.contrib.messages.error')
-    def test_create_user_email_exists(self, mock_message):
-        test_user = User.objects.create_user(
-            email='existinguser@example.com',
-            password='password123',
-            fname='Existing',
-            lname='User',
-            phone_number='9876543210',
-            address='123 Main St'
-        )
-        test_user.save()
-
-        post_data = {
-            'email': 'existinguser@example.com',  # Email already exists
-            'fname': 'John',
-            'lname': 'Doe',
-            'role': 'TA',
-            'dept': 'Computer Science',
-            'phone_number': '1234567890',
-            'address1': '123 Main St',
-            'address2': 'Some City',
-            'password': 'password123',
-        }
-
-        request = MagicMock()
-        request.POST = post_data
-
-        create_user_account(request)
-
-        # Assert that an error message is generated for existing email
-        mock_message.assert_called_with(request, "Email already exists in the system")
-
-    @patch('django.contrib.messages.error')
-    def test_create_user_no_password(self, mock_message):
-        post_data = {
-            'email': 'newuser@example.com',
-            'fname': 'John',
-            'lname': 'Doe',
-            'role': 'TA',
-            'dept': 'Computer Science',
-            'phone_number': '1234567890',
-            'address1': '123 Main St',
-            'address2': 'Some City',
-        }
-
-        request = MagicMock()
-        request.POST = post_data
-
-        create_user_account(request)
-
-        # Assert that an error message is generated for no password
-        mock_message.assert_called_with(request, "Must create a password")
-
-    @patch('django.contrib.messages.error')
-    def test_create_user_email_password_only(self, mock_message):
-        post_data = {
-            'email': 'newuser@example.com',
-            'password': 'password123',
-        }
-
-        request = MagicMock()
-        request.POST = post_data
-
-        create_user_account(request)
-
-        user = User.objects.get(email='newuser@example.com')
-
-        #Check password
-        self.assertTrue(user.check_password('password123'))
-        # Assert other fields are empty
-        self.assertEqual(user.fname, 'None')
-        self.assertEqual(user.lname, 'None')
-
-        # Assert no errors in case of success
-        mock_message.assert_not_called()
-
-
-class AccountManagementEditTests(TestCase):
-
-    def setUp(self):
-        self.ta_user = User.objects.create_user(
-            email='ta@example.com',
-            password='tapassword123',
-            fname='TA',
-            lname='User',
-            phone_number='9876543210',
-            address='456 TA Lane'
-        )
-        self.ta_user.save()
-        self.ta_user = TA(user=self.ta_user, ta_dept="dept")
-        self.ta_user.save()
-
-    @patch('django.contrib.messages.error')
-    def test_edit_user_change_name(self, mock_message):
-        post_data = {
-            'id': self.ta_user.id,
-            'old_role': 'TA',
-            'role':'TA',
-            'fname': 'Updated',
-        }
-
-        request = MagicMock()
-        request.POST = post_data
-
-        edit_user_account(request)
-
-        user = User.objects.get(self.ta_user.id)
-
-        # Assert that the user's details are updated
-        user.refresh_from_db()
-        self.assertEqual(user.fname, 'Updated')
+        # Assert course is created
+        self.assertEqual(course.course_name, 'Test Course')
+        self.assertEqual(course.course_identifier, '150')
+        self.assertEqual(course.instructor, self.instructor_user)
 
         # Assert no errors in case of success
         mock_message.assert_not_called()
 
     @patch('django.contrib.messages.error')
-    def test_edit_user_change_password(self, mock_message):
+    def test_create_course_no_identifier(self, mock_message):
         post_data = {
-            'id': self.ta_user.id,
-            'old_role': 'TA',
-            'role': 'TA',
-            'password': 'password456',
+            'course_name': 'Test Course',
+            'course_identifier': '600',
+            'course_dept': 'Computer Science',
+            'course_credits': 3,
+            'instructor': self.instructor_user.user
+
         }
 
         request = MagicMock()
         request.POST = post_data
+        request.user = self.supervisor_user.user
 
-        edit_user_account(request)
+        create_course(request)
 
-        user = User.objects.get(self.ta_user.id)
-
-        user.refresh_from_db()
-        # Check password
-        self.assertTrue(user.check_password('password456'))
-
-        # Assert no errors in case of success
-        mock_message.assert_not_called()
+        mock_message.assert_called_with(request, f"A course with the identifier '{post_data['course_identifier']}' already exists.")
 
     @patch('django.contrib.messages.error')
-    def test_edit_user_change_email(self, mock_message):
+    def test_edit_course_success(self, mock_message):
         post_data = {
-            'id': self.ta_user.id,
-            'old_role': 'TA',
-            'role': 'TA',
-            'email': 'newemail@example.com'
-        }
-
-        request = MagicMock()
-        request.POST = post_data
-
-        edit_user_account(request)
-
-        # Assert that an error message is generated when attempting to change email
-        mock_message.assert_called_with(request, "Cannot change email, create a new user instead")
-
-    @patch('django.contrib.messages.error')
-    def test_edit_user_change_role(self, mock_message):
-        post_data = {
-            'id': self.ta_user.id,
-            'old_role': 'TA',
-            'role': 'Supervisor',
-            'password': 'password456',
-        }
-
-        request = MagicMock()
-        request.POST = post_data
-
-        edit_user_account(request)
-
-        user = User.objects.get(self.ta_user.id)
-
-        user.refresh_from_db()
-        # Check password
-        self.assertEqual(user.role, 'Supervisor')
-
-        # Assert no errors in case of success
-        mock_message.assert_not_called()
-
-class AccountManagementDeleteTests(TestCase):
-
-    def setUp(self):
-        self.ta_user = User.objects.create_user(
-            email='ta@example.com',
-            password='tapassword123',
-            fname='TA',
-            lname='User',
-            phone_number='9876543210',
-            address='456 TA Lane'
-        )
-        self.ta_user = TA.objects.create(user=self.ta_user, ta_dept='TADepartment')
-        self.ta_user.save()
-
-    @patch('django.contrib.messages.error')
-    def test_delete_nothing(self, mock_message):
-        post_data = {
+            'course_dept': 'Robotics',
+            'course_credits': 3,
+            'instructor': self.instructor_user.user
 
         }
 
         request = MagicMock()
         request.POST = post_data
+        request.user = self.supervisor_user.user
 
-        delete_user_account(request)
+        edit_course(request, self.course_test.course_id,)
 
-        user = User.objects.get('ta@example.com')
+        course = Course.objects.get(course_name='CS600')
 
-        self.assertIsNotNone(user)
-
-        # Assert no errors in case of success
-        mock_message.assert_not_called()
-
-    @patch('django.contrib.messages.error')
-    def test_delete_user_success(self, mock_message):
-        useremail = self.ta_user.user
-        useremail = useremail.email
-        post_data = {
-            'email': useremail,
-        }
-
-        request = MagicMock()
-        request.POST = post_data
-
-        delete_user_account(request)
-
-        user = User.objects.filter(email='ta@example.com').exists()
-
-        self.assertFalse(user)
-
-        # Assert no errors in case of success
-        mock_message.assert_not_called()
-
-    @patch('django.contrib.messages.error')
-    def test_delete_user_does_not_exist(self, mock_message):
-        post_data = {
-            'id': 'email@example.com',
-        }
-
-        request = MagicMock()
-        request.POST = post_data
-
-        delete_user_account(request)
-
-        # Assert that an error message is generated when attempting to change email
-        mock_message.assert_called_with(request, "User does not exist")
+        # Assert course is created
+        self.assertEqual(course.course_name, 'Test Course')
+        self.assertEqual(course.course_identifier, '150')
