5cf1334fecd75ec21a172796d39e1b9de21e9049
kaye-s <kayes@uwm.edu>
2024-12-18 19:05:11 -0600

    more testing updates and two new testing files

--- TAScheduler/tests/AcceptanceTests/test_create_courses_with_instructor.py
+++ TAScheduler/tests/AcceptanceTests/test_create_courses_with_instructor.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from TAScheduler.models import Course, Supervisor, TA, Instructor, Section
+from TAScheduler.models import Course, Supervisor, TA, Instructor
 from django.contrib.auth.models import Group
 from django.contrib.messages import get_messages
 
@@ -59,46 +59,7 @@ class CreateCourseTests(TestCase):
         )
         self.course_test.save()
 
-    def test_create_section_no_error_no_users(self):
-        self.client.login(email='supervisor@example.com', password='superpassword123')
-
-        response = self.client.get("/courses_supervisor/")
-        self.assertEqual(response.status_code, 200)
-
-        data = {
-            'section_type': 'Lab',
-            'section_num': '600',
-            'section_course': self.course_test.course_id,
-            'days_of_week': 'MW',
-            'section_startTime': '8:00AM',
-            'section_endTime': '10:00AM',
-            'action': 'createSection',
-        }
-        response = self.client.post("/courses_supervisor/", data)
-        self.assertEqual(response.status_code, 302)
-
-        # Check if the course was created successfully
-        new_section = Section.objects.get(section_num='600')
-        self.assertEqual(new_section.section_type, 'Test Course')
-        self.assertEqual(new_section.section_num, '600')
-        self.assertEqual(new_section.section_course, 'Computer Science')
-        self.assertEqual(new_section.days_of_week, None)
-        self.assertEqual(new_section.section_startTime, None)
-        self.assertEqual(new_section.section_endTime, None)
-
-    def test_instructor_access_course_management(self):
-        self.client.login(email='instructor@example.com', password='instructorpassword123')
-
-        response = self.client.get("/courses_supervisor/")
-        self.assertEqual(response.status_code, 403)
-
-    def test_ta_access_course_management(self):
-        self.client.login(email='ta@example.com', password='tapassword123')
-
-        response = self.client.get("/courses_supervisor/")
-        self.assertEqual(response.status_code, 403)
-
-    def test_course_identifier_must_be_unique(self):
+    def test_create_course_no_error_instructor(self):
         self.client.login(email='supervisor@example.com', password='superpassword123')
 
         response = self.client.get("/courses_supervisor/")
@@ -106,23 +67,15 @@ class CreateCourseTests(TestCase):
 
         data = {
             'course_name': 'Test Course',
-            'course_identifier': '150',
+            'course_identifier': '600',
             'course_dept': 'Computer Science',
             'course_credits': 3,
+            'instructor': self.instructor_user.id,
             'action': 'createCourse',
         }
         response = self.client.post("/courses_supervisor/", data)
         self.assertEqual(response.status_code, 302)
 
+        # Check if the course was created successfully
         new_course = Course.objects.get(course_name='Test Course')
-        messages = list(get_messages(response.wsgi_request))
-
-        self.assertTrue(
-            any(f"A course with the identifier '{new_course.course_identifier}' already exists." in str(message) for message in
-                messages),
-            "Expected a message - Course ID already exists")
-        courses = Course.objects.filter(course_identifier='150')
-        self.assertEqual(courses.count(), 1)
-
-
-
+        self.assertEqual(new_course.instructor, self.instructor_user.id)
